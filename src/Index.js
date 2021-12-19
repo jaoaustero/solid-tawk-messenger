a9afa6b6928466f43003343fc9d73de402ca342f
Jerald Austero <jerald.austero0229@gmail.com>
2021-12-19 15:38:41 +0800

    setup the build configuration

---
+++ src/Index.js
@@ -0,0 +1,238 @@
+// Dependency
+import { onMount, onCleanup, mergeProps } from "solid-js";
+
+// Utilities
+import { isValidString } from "./utils/helper";
+import { loadScript } from "./utils/widget";
+
+const TawkMessenger = (props) => {
+    const merged = mergeProps(
+        {
+            /**
+             * Default properties
+             */
+            propertyId : '',
+            widgetId : '',
+
+            /**
+             * Custom style
+             */
+            customStyle: {},
+
+            /**
+             * Callbacks
+             */
+            onLoad : () => {},
+            onStatusChange : () => {},
+            onBeforeLoad : () => {},
+            onChatMaximized : () => {},
+            onChatMinimized : () => {},
+            onChatHidden : () => {},
+            onChatStarted : () => {},
+            onChatEnded : () => {},
+            onPrechatSubmit : () => {},
+            onOfflineSubmit : () => {},
+            onChatMessageVisitor : () => {},
+            onChatMessageAgent : () => {},
+            onChatMessageSystem : () => {},
+            onAgentJoinChat : () => {},
+            onAgentLeaveChat : () => {},
+            onChatSatisfaction : () => {},
+            onVisitorNameChanged : () => {},
+            onFileUpload : () => {},
+            onTagsUpdated : () => {}
+        },
+        props
+    );
+
+
+    onMount(() => {
+        load();
+    });
+
+
+    onCleanup(() => {
+        delete window.Tawk_API;
+        delete window.Tawk_LoadStart;
+    });
+
+
+    const load = () => {
+        if (!isValidString(String, merged.propertyId)) {
+			console.error('[Tawk-messenger-solid warn]: You didn\'t specified \'propertyId\' property in the plugin.');
+			return;
+		}
+	
+		if (!isValidString(String, merged.widgetId)) {
+            console.error('[Tawk-messenger-solid warn]: You didn\'t specified \'widgetId\' property in the plugin.');
+			return;
+		}
+
+		if (!window || !document) {
+			return;
+        }
+        
+        init();
+    }
+
+
+    const init = () => {
+        /**
+		 * Set placeholder
+		 */
+        window.Tawk_API = window.Tawk_API || {};
+        window.Tawk_LoadStart = new Date();
+
+		/**
+		 * Inject the Tawk script
+		 */
+        loadScript({
+            propertyId : merged.propertyId,
+            widgetId : merged.widgetId
+        });
+
+        mapCallbacks();
+    }
+
+    
+    /**
+	 * API for listening an event emitting
+	 * inside of the widget
+	 */
+    const mapCallbacks = () => {
+        window.addEventListener('tawkLoad', () => {
+			merged.onLoad();
+		});
+
+        window.addEventListener('tawkStatusChange', (status) => {
+			merged.onStatusChange(status.detail);
+		});
+
+		window.addEventListener('tawkBeforeLoad', () => {
+			merged.onBeforeLoad();
+		});
+
+		window.addEventListener('tawkChatMaximized', () => {
+			merged.onChatMaximized();
+		});
+
+		window.addEventListener('tawkChatMinimized', () => {
+			merged.onChatMinimized();
+		});
+
+		window.addEventListener('tawkChatHidden', () => {
+			merged.onChatHidden();
+		});
+
+		window.addEventListener('tawkChatStarted', () => {
+			merged.onChatStarted();
+		});
+
+		window.addEventListener('tawkChatEnded', () => {
+			merged.onChatEnded();
+		});
+
+		window.addEventListener('tawkPrechatSubmit', (data) => {
+			merged.onPrechatSubmit(data.detail);
+		});
+
+		window.addEventListener('tawkOfflineSubmit', (data) => {
+			merged.onOfflineSubmit(data.detail);
+		});
+		
+		window.addEventListener('tawkChatMessageVisitor', (message) => {
+			merged.onChatMessageVisitor(message.detail);
+		});
+
+		window.addEventListener('tawkChatMessageAgent', (message) => {
+			merged.onChatMessageAgent(message.detail);
+		});
+
+		window.addEventListener('tawkChatMessageSystem', (message) => {
+			merged.onChatMessageSystem(message.detail);
+		});
+
+		window.addEventListener('tawkAgentJoinChat', (data) => {
+			merged.onAgentJoinChat(data.detail);
+		});
+		
+		window.addEventListener('tawkAgentLeaveChat', (data) => {
+			merged.onAgentLeaveChat(data.detail);
+		});
+
+		window.addEventListener('tawkChatSatisfaction', (satisfaction) => {
+			merged.onChatSatisfaction(satisfaction.detail);
+		});
+		
+		window.addEventListener('tawkVisitorNameChanged', (visitorName) => {
+			merged.onVisitorNameChanged(visitorName.detail);
+		});
+
+		window.addEventListener('tawkFileUpload', (link) => {
+			merged.onFileUpload(link.detail);
+		});
+
+		window.addEventListener('tawkTagsUpdated', (data) => {
+			merged.onTagsUpdated(data.detail);
+		});
+    };
+
+
+    /**
+	 * API for calling an action on the widget
+	 */
+    const mapActions = {
+        maximize : () => window.Tawk_API.maximize(),
+		minimize : () => window.Tawk_API.minimize(),
+		toggle : () => window.Tawk_API.toggle(),
+		popup : () => window.Tawk_API.popup(),
+		showWidget : () => window.Tawk_API.showWidget(),
+		hideWidget : () => window.Tawk_API.hideWidget(),
+		toggleVisibility : () => window.Tawk_API.toggleVisibility(),
+		endChat : () => window.Tawk_API.endChat()
+    };
+
+
+    /**
+	 * API for setting a data on the widget
+	 */
+    const mapGetters = {        
+        getWindowType : () => window.Tawk_API.getWindowType(),
+		getStatus : () => window.Tawk_API.getStatus(),
+		isChatMaximized : () => window.Tawk_API.isChatMaximized(),
+		isChatMinimized : () => window.Tawk_API.isChatMinimized(),
+		isChatHidden : () => window.Tawk_API.isChatHidden(),
+		isChatOngoing : () => window.Tawk_API.isChatOngoing(),
+		isVisitorEngaged : () => window.Tawk_API.isVisitorEngaged(),
+		onLoaded : () => window.Tawk_API.onLoaded
+    };
+
+
+    /**
+	 * API for setting a data on the widget
+	 */
+    const mapSetters = {
+        visitor : (data) => window.Tawk_API.visitor = data,
+		setAttributes : (attribute, callback) => window.Tawk_API.setAttributes(attribute, callback),
+		addEvent : (event, metadata, callback) => window.Tawk_API.addEvent(event, metadata, callback),
+		addTags : (tags, callback) => window.Tawk_API.addTags(tags, callback),
+		removeTags : (tags, callback) => window.Tawk_API.removeTags(tags, callback),
+    };
+
+
+    /**
+     * Expose the Tawk API functions
+     */
+    merged.ref(
+        {
+            ...mapGetters,
+            ...mapActions,
+            ...mapSetters
+        }
+    );
+
+
+    return null;
+};
+
+export default TawkMessenger;
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
+const path = require('path')
+const { defineConfig } = require('vite');
+const solidPlugin = require('vite-plugin-solid');
 
 export default defineConfig({
-	plugins: [solidPlugin()],
-	build: {
-		target: "esnext",
-		polyfillDynamicImport: false,
-	},
+	plugins : [solidPlugin()],
+	build : {
+		lib : {
+			entry : path.resolve(__dirname, 'src/index.js'),
+			name : 'solid-tawk-messenger',
+			fileName : ($format) => `solid-tawk-messenger.${$format}.js`
+		}
+	}
 });
